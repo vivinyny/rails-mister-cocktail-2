397f65825aecaf253762d67f2725b027082a3162
Vilouny Phoubandith <vilouny.ph@gmail.com>
2020-05-16 17:31:40 +0200

    controller cocktail valid

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,10 +1,19 @@
 class CocktailsController < ApplicationController
   def index
+    @cocktails = Cocktail.all
   end
 
   def show
+    @cocktail = Cocktail.find(params[:id])
   end
 
   def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new
+    @cocktail.save
+    redirect_to cocktails_path
   end
 end
